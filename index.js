418a558ad76b31e1a4a29a9852a58a94b9edcf17
Mushfiquzzaman Liyon <farhanliyon308@gmail.com>
2022-12-06 18:00:52 +0600

    Update

--- index.js
+++
@@ -1,6 +0,0 @@
-import {countryList} from "./countries.js";
-countryList.map((all_item,i)=>{
-    let country_Details=`My country name is ${all_item.country_name}`;
-    let code_Details=`My country code is ${all_item.country_code}`;
-    console.log(country_Details+" && "+code_Details);
-})
